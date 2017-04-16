d802d7405117fa3c72b1f4bed0bcdc4a285c837f
zengyanling <939184663@qq.com>
2017-04-16 20:58:08 +0800

    update: docs

--- docs/test.js
+++ docs/test.js
@@ -1,3 +1,37 @@
+//////////// mid1.png
+
+
+function n1(store){
+		return function(next){
+				return function(action) {
+						console.log('n1 begin');
+						next(action);
+						console.log('n1 end');
+				};
+		};
+}
+function n2(store){
+		return function(next){
+				return function(action) {
+						console.log('n2 begin');
+						next(action);
+						console.log('n2 end');
+				};
+		};
+}
+function applyMiddlewareSimilar(store, middlewares) {
+		middlewares = middlewares.slice();
+		middlewares.reverse();
+
+		let dispatch = store.dispatch;
+		// 在每一个 middleware 中变换 dispatch 方法。
+		middlewares.forEach(middleware =>
+				dispatch = middleware(store)(dispatch)
+		);
+		return Object.assign({}, store, { dispatch })
+}
+applyMiddlewareSimilar(store, n1, n2)
+store.dispatch('EE');
 //////////// mid2.png
 // action.js
 var newsList = ({newsId,url,page})=> {
@@ -106,3 +140,50 @@ const styles = StyleSheet.create({
 	},
 	...
 });
+
+////////////////////hoc4.png
+// formSubscription
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+const formSubscription = formKey => WrappedComponent => {
+	class formCommon extends Component {
+		constructor(props){
+			super(props);
+			this.state = {
+				error: '',
+			};
+		}
+		validate = (key) => {
+      // 处理 this.state.error
+		}
+		submit = (submitFn) => {
+			if (this.validate()){
+				const { handleSubmit } = this.props;
+				// 进行表单验证
+				handleSubmit(submitFn)();
+			}
+		}
+		render() {
+			return (<WrappedComponent
+				setFormState={this.setFormState}
+				formSubmit={this.submit.bind(this)}
+				validateHandler={this.validate}
+				{...this.props}
+				{...this.state}
+			/>);
+		}
+	}
+	const connectForm = connect(
+		state => {
+			const formValidate = state.form[formKey] || {};
+			return {
+				formValidate,
+			};
+},
+	)(formCommon);
+	return reduxForm({
+		form: formKey,
+	})(connectForm);
+};
+export default formSubscription; 
